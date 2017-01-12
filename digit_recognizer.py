57fc22bc923f155845a9dfb1fd1f7cd53ca2a78a
Jeremy Crowe <crowe.jb@gmail.com>
2017-01-11 21:32:02 -0800

    remove csv data and use tf dataset

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -26,8 +26,7 @@ data = pd.read_csv('train.csv')
 
 print('data({0[0]},{0[1]})'.format(data.shape))
 print (data.head())
-import pdb
-pdb.set_trace()
+
 images = data.iloc[:,1:].values
 images = images.astype(np.float)
 
@@ -41,5 +40,38 @@ print ('image_size => {0}'.format(image_size))
 
 # in this case all images are square
 image_width = image_height = np.ceil(np.sqrt(image_size)).astype(np.uint8)
+labels_flat = data[[0]].values.flatten()
 
-print ('image_width => {0}\nimage_height => {1}'.format(image_width,image_height))
+print('labels_flat({0})'.format(len(labels_flat)))
+print ('labels_flat[{0}] => {1}'.format(IMAGE_TO_DISPLAY,labels_flat[IMAGE_TO_DISPLAY]))
+labels_count = np.unique(labels_flat).shape[0]
+
+
+# convert class labels from scalars to one-hot vectors
+# 0 => [1 0 0 0 0 0 0 0 0 0]
+# 1 => [0 1 0 0 0 0 0 0 0 0]
+# ...
+# 9 => [0 0 0 0 0 0 0 0 0 1]
+def dense_to_one_hot(labels_dense, num_classes):
+    num_labels = labels_dense.shape[0]
+    index_offset = np.arange(num_labels) * num_classes
+    labels_one_hot = np.zeros((num_labels, num_classes))
+    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+    return labels_one_hot
+
+# labels = dense_to_one_hot(labels_flat, labels_count)
+# labels = labels.astype(np.uint8)
+
+# print('labels({0[0]},{0[1]})'.format(labels.shape))
+# print ('labels[{0}] => {1}'.format(IMAGE_TO_DISPLAY,labels[IMAGE_TO_DISPLAY]))
+
+import pdb
+pdb.set_trace()
+
+indices = labels_flat
+depth = labels_count
+on_value = 1
+off_value = 0
+axis = -1
+
+tf.one_hot(indices, depth, on_value, off_value, axis)
