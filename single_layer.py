9a497de1a983e57dc23fd9a93a09ae96b3acec67
Jeremy Crowe <crowe.jb@gmail.com>
2017-01-12 16:33:24 -0800

    implementing session and running optimize

--- single_layer.py
+++ single_layer.py
@@ -33,16 +33,38 @@ biases = tf.Variable(tf.zeros([num_classes]))
 # Generate logits
 logits = tf.matmul(X, weights) + biases
 predicted = tf.nn.softmax(logits)
-predicted_cls = tf.argmax(predicted), dimension=1)
+predicted_cls = tf.argmax(predicted, dimension=1)
 
-# Calculate cross entropy
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits,
-                                                        labels=y_true_one_hot)
-
-# Generate cost function by calculating mean of cross entropy
-cost_fn = tf.reduce_mean(cross_entropy)
+# Calculate cross entropy + cost function
+cost_fn = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(logits=logits,
+                                                    labels=y_one_hot))
 
 # Using gradient decent to optimize our single layer
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.5).minimize(cost_fn)
+optimizer = tf.train.GradientDescentOptimizer(
+                learning_rate=0.5).minimize(cost_fn)
+
 correct_prediction = tf.equal(predicted_cls, y_true)
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    batch_size = 100
+
+    def optimize(num_iterations):
+        for i in range(num_iterations):
+            x_batch, y_true_batch = data.train.next_batch(batch_size)
+
+            feed_dict_train = {X: x_batch,
+                               y_one_hot: y_true_batch}
+
+            sess.run(optimizer, feed_dict=feed_dict_train)
+
+    optimize(num_iterations=1)
+    feed_dict_test = {X: data.test.images,
+                      y_one_hot: data.test.labels,
+                      predicted_cls: data.test.cls}
+
+    acc = sess.run(accuracy, feed_dict=feed_dict_test)
+    print("Accuracy on test-set: {0:.1%}".format(acc))
