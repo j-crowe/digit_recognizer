7bc8c3783a37393df5b5e5997f6b39789139daf3
Jeremy Crowe <crowe.jb@gmail.com>
2019-01-15 18:01:04 -0800

    testing

--- single_layer.py
+++ single_layer.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # Load in mnist dataset using one_hot
 from tensorflow.examples.tutorials.mnist import input_data
-data = input_data.read_data_sets("data/MNIST/", one_hot=True)
+data = input_data.read_data_sets("/tmp/MNIST/", one_hot=True)
 
 # Get basic information about dataset
 print("Training Size:\t\t{}".format(len(data.train.labels)))
