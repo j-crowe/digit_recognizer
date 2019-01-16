411ce043f244a48f4cdcabecb3f4f05e679c5d10
Jeremy Crowe <crowe.jb@gmail.com>
2019-01-16 13:59:04 -0800

    syntax

--- convolutional.py
+++ convolutional.py
@@ -153,5 +153,4 @@ print("test accuracy %g" % accuracy.eval(feed_dict={
     x: mnist.test.images, y_true: mnist.test.labels}))
 
 f = open("/tmp/accuracy.txt", "a")
-f.write(accuracy.eval( accuracy.eval(feed_dict={
-    x: mnist.test.images, y_true: mnist.test.labels}))
+f.write(accuracy.eval( accuracy.eval(feed_dict={x: mnist.test.images, y_true: mnist.test.labels}))
